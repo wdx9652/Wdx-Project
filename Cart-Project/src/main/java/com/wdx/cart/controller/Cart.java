81a1f79fa68848e35841ccd5aa3077e60d0f669c
Dicex <204503737@qq.com>
2021-06-28 08:37:29 +0800

    修改了cart类

--- Cart-Project/src/main/java/com/wdx/cart/controller/Cart.java
+++ Cart-Project/src/main/java/com/wdx/cart/controller/Cart.java
@@ -1,4 +1,18 @@
 package com.wdx.cart.controller;
 
 public class Cart {
+
+    String name;
+    String age;
+
+
+    public Cart(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Cart cart = new Cart("A货","123");
+
+    }
 }
